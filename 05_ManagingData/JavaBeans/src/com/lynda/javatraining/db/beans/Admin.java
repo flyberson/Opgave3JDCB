afbb422840b0c8f57f579bd33ff1db7c4b655690
flyberson <flyberson12@gmail.com>
2017-02-20 18:08:25 +0100

    added 5

---
+++ 05_ManagingData/JavaBeans/src/com/lynda/javatraining/db/beans/Admin.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lynda.javatraining.db.beans;
+
+/**
+ *
+ * @author Martin Hilaire
+ */
+public class Admin {
+    private int adminId;
+    private String userName;
+    private String password;
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
