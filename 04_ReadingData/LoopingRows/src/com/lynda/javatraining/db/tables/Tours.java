75f39a5423e4f0d9ed33f9d4f4aef9bb63cc9e1c
flyberson <flyberson12@gmail.com>
2017-02-20 18:02:04 +0100

    Add files via upload

---
+++ 04_ReadingData/LoopingRows/src/com/lynda/javatraining/db/tables/Tours.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lynda.javatraining.db.tables;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.NumberFormat;
+
+/**
+ *
+ * @author Martin Hilaire
+ */
+public class Tours {
+    
+    public static void displayData(ResultSet rs) throws SQLException{
+        while (rs.next()) {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("Tour "+rs.getInt("tourId")+":");
+            buffer.append(rs.getString("tourName"));
+            
+            double price= rs.getDouble("price");
+            NumberFormat nf = NumberFormat.getCurrencyInstance();
+            String formattedPrice = nf.format(price);
+            buffer.append(" (" + formattedPrice + " ) ");
+            System.out.println(buffer.toString());
+            
+        }
+    }
+
+   
+}
